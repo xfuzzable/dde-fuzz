ee2529297905cb8f9697e0343003d364b64ce977
YingchaoLiu <liuyingchao@shuimuyulin.com>
2022-03-18 12:29:13 +0800

    增加 dde app 全部安装功能

--- install.sh
+++ install.sh
@@ -1,11 +1,42 @@
-
 #!/bin/bash
 
-for i in "$@";
-do
-    echo "install $i"
-    apt source $i
-    apt build-dep $i
-done
+apps=(dde-calendar
+    dde-clipboard
+    dde-control-center
+    dde-device-formatter
+    dde-dock
+    dde-file-manager
+    dde-file-manager-integration
+    dde-introduction
+    dde-kwin
+    dde-launcher
+    dde-network-utils
+    dde-polkit-agent
+    dde-printer
+    dde-qt-dbus-factory
+    dde-session
+    dde-session-ui
+    dde-session-shell
+    dde-wayland
+)
+
+if [ $# == 0 ]
+then
+    for app in ${apps[*]};
+    do 
+        echo "install $app"
+        apt source $app -y
+        apt build-dep $app -y
+    done
+else
+    for app in "$@";
+    do
+        echo "install $app"
+        apt source $app -y 
+        apt build-dep $app -y 
+    done
+fi
+
+
 
 
