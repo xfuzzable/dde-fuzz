6caac20218f8802387e28715cd756dffc41440b1
YingchaoLiu <liuyingchao@shuimuyulin.com>
2022-03-21 10:21:00 +0800

    修复 build.sh bug

--- fuzz-dde-device-formatter/build.sh
+++ fuzz-dde-device-formatter/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Project Paht is $@"
+python3 getPath.py $@
 mkdir -p build
 cd build
-qmake $@
-make CC=xfuzz-cc CXX=xfuzz-c++ LINK=xfuzz-c++ VERBOSE=1 -j
+qmake ..
+make CC=xfuzz-cc CXX=xfuzz-c++ LINK=xfuzz-c++ V=1 -j
