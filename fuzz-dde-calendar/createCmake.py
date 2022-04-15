7879871ddd6dd6277d6408382eb59c0df2db6698
YingchaoLiu <1839705275@qq.com>
2022-04-15 18:28:23 +0800

    更新 dde-calendar 驱动

--- fuzz-dde-calendar/createCmake.py
+++ fuzz-dde-calendar/createCmake.py
@@ -2,54 +2,112 @@
 import os 
 import sys 
 
+
+# cmake_minimum_required(VERSION 3.7)
+# project(dde-calendar-xfuzz)
+
+# find_package(PkgConfig REQUIRED)
+# find_package(Qt5 COMPONENTS
+#     Core
+#     DBus
+# REQUIRED)
+
+# set(CMAKE_CXX_STANDARD 11)
+# set(CMAKE_INCLUDE_CURRENT_DIR ON)
+# set(CMAKE_AUTOMOC ON)
+
+# #安全编译参数
+# set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fstack-protector-strong -D_FORTITY_SOURCE=1 -z noexecstack -pie -fPIC -z lazy")
+# set(PROJECT_PATH dde-calendar-abspath)
+# include_directories(${PROJECT_PATH}/calendar-basicstruct/src)
+# file(GLOB_RECURSE BASESTRUCT_SRCS ${PROJECT_PATH}/calendar-basicstruct/src/*.cpp)
+
+# add_executable(${PROJECT_NAME} main.cpp ${BASESTRUCT_SRCS})
+
+# target_include_directories(${PROJECT_NAME} PUBLIC
+#     ${PROJECT_PATH}/calendar-basicstruct/src
+#     ${DtkWidget_INCLUDE_DIRS}
+#     ${XCB_EWMH_INCLUDE_DIRS}
+#     ${DFrameworkDBus_INCLUDE_DIRS}
+#     ${Qt5Gui_PRIVATE_INCLUDE_DIRS}
+#     ${PROJECT_BINARY_DIR}
+#     ${QGSettings_INCLUDE_DIRS}
+#     ${Gio-2.0_INCLUDE_DIRS}
+#     ${Qt5X11Extras_INCLUDE_DIRS}
+# )
+# target_link_libraries(${PROJECT_NAME} PRIVATE
+#     ${XCB_EWMH_LIBRARIES}
+#     ${DFrameworkDBus_LIBRARIES}
+#     ${DtkWidget_LIBRARIES}
+#     ${Qt5Widgets_LIBRARIES}
+#     ${Qt5Concurrent_LIBRARIES}
+#     ${Qt5X11Extras_LIBRARIES}
+#     ${Qt5DBus_LIBRARIES}
+#     ${Qt5Multimedia_LIBRARIES}
+#     ${QGSettings_LIBRARIES}
+#     ${Qt5Svg_LIBRARIES}
+#     ${DEEPIN_PW_CHECK}
+#     ${SHMN_VIDEO}
+#     ${LIBS}
+# )
+
 cmakeLists = """
 cmake_minimum_required(VERSION 3.7)
-project(dde-calendar-xfuzz)
-
-find_package(PkgConfig REQUIRED)
-find_package(Qt5 COMPONENTS
-    Core
-    DBus
-REQUIRED)
+project(dde-calendar-service-xfuzz)
 
 set(CMAKE_CXX_STANDARD 11)
 set(CMAKE_INCLUDE_CURRENT_DIR ON)
 set(CMAKE_AUTOMOC ON)
+set(PROJECT_PATH dde-calendar-abspath)
 
 #安全编译参数
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fstack-protector-strong -D_FORTITY_SOURCE=1 -z noexecstack -pie -fPIC -z lazy")
-set(PROJECT_PATH dde-calendar-abspath)
-include_directories(${PROJECT_PATH}/calendar-basicstruct/src)
-file(GLOB_RECURSE BASESTRUCT_SRCS ${PROJECT_PATH}/calendar-basicstruct/src/*.cpp)
 
-add_executable(${PROJECT_NAME} main.cpp ${BASESTRUCT_SRCS})
+set(APP_RES_DIR "${PROJECT_PATH}/calendar-service/assets")
+set(APP_SERVICE "${APP_RES_DIR}/data/com.deepin.dataserver.Calendar.service")
+set(AUTOSTART_DESKTOP "${APP_RES_DIR}/dde-calendar-service.desktop")
+set(HUANGLIDB "${APP_RES_DIR}/data/huangli.db")
+set(APP_QRC "${APP_RES_DIR}/resources.qrc")
 
-target_include_directories(${PROJECT_NAME} PUBLIC
-    ${PROJECT_PATH}/calendar-basicstruct/src
-    ${DtkWidget_INCLUDE_DIRS}
-    ${XCB_EWMH_INCLUDE_DIRS}
-    ${DFrameworkDBus_INCLUDE_DIRS}
-    ${Qt5Gui_PRIVATE_INCLUDE_DIRS}
-    ${PROJECT_BINARY_DIR}
-    ${QGSettings_INCLUDE_DIRS}
-    ${Gio-2.0_INCLUDE_DIRS}
-    ${Qt5X11Extras_INCLUDE_DIRS}
+
+# Find the library
+find_package(PkgConfig REQUIRED)
+find_package(DtkCore REQUIRED)
+find_package(Qt5 COMPONENTS
+    Core
+    DBus
+    Sql
+REQUIRED)
+
+set(LINK_LIBS
+    Qt5::Core
+    Qt5::DBus
+    Qt5::Sql
+    ${DtkCore_LIBRARIES}
 )
-target_link_libraries(${PROJECT_NAME} PRIVATE
-    ${XCB_EWMH_LIBRARIES}
-    ${DFrameworkDBus_LIBRARIES}
-    ${DtkWidget_LIBRARIES}
-    ${Qt5Widgets_LIBRARIES}
-    ${Qt5Concurrent_LIBRARIES}
-    ${Qt5X11Extras_LIBRARIES}
-    ${Qt5DBus_LIBRARIES}
-    ${Qt5Multimedia_LIBRARIES}
-    ${QGSettings_LIBRARIES}
-    ${Qt5Svg_LIBRARIES}
-    ${DEEPIN_PW_CHECK}
-    ${SHMN_VIDEO}
-    ${LIBS}
-)
+
+include_directories(${PROJECT_PATH}/calendar-service/src)
+include_directories(${PROJECT_PATH}/calendar-service/src/dbmanager)
+include_directories(${PROJECT_PATH}/calendar-service/src/dbus)
+include_directories(${PROJECT_PATH}/calendar-service/src/lunarandfestival)
+include_directories(${PROJECT_PATH}/calendar-service/src/pinyin)
+include_directories(${PROJECT_PATH}/calendar-basicstruct)
+
+file(GLOB_RECURSE CALENDARSERVICE_SRCS ${PROJECT_PATH}/calendar-service/src/*.cpp ${PROJECT_PATH}/calendar-basicstruct/src/*.cpp)
+list(REMOVE_ITEM CALENDARSERVICE_SRCS "${PROJECT_PATH}/calendar-service/src/main.cpp")
+
+#添加资源文件
+QT5_ADD_RESOURCES(CALENDARSERVICE_SRCS ${APP_QRC})
+
+add_executable(${PROJECT_NAME} main.cpp ${CALENDARSERVICE_SRCS})
+target_link_libraries(${PROJECT_NAME} ${LINK_LIBS} basestruct)
+
+set(CMAKE_INSTALL_PREFIX /usr)
+# Install files
+install(TARGETS ${PROJECT_NAME} DESTINATION lib/deepin-daemon/)
+install(FILES ${APP_SERVICE} DESTINATION share/dbus-1/services/)
+install(FILES ${AUTOSTART_DESKTOP} DESTINATION /etc/xdg/autostart/)
+install(FILES ${HUANGLIDB} DESTINATION share/dde-calendar/data/)
 """
 
 
--- fuzz-dde-calendar/main.cpp
+++ fuzz-dde-calendar/main.cpp
@@ -1,34 +1,155 @@
 
-#include <QString>
+// #include <QString>
+// #include <QDebug>
+// #include <QByteArray>
+// #include "utils.h"
+// #include <QJsonDocument>
+// #include <QJsonArray>
+// #include "xfuzz.h"
+
+// //自定义转换函数，输入是任意数量的支持的类型（包括其他自定义类型）
+// QString to_qstring(std::string &s) {
+//     return QString(s.c_str());
+// }
+
+// XFUZZ_CUSTOM_CONVERTER(to_qstring);
+
+// void test(QString &s) {
+//     qInfo() << s;
+//     Utils * ut = new Utils();
+//     auto qdt = ut->fromconvertData(s);
+//     qInfo() << qdt;
+//     auto qs = ut->toconvertData(qdt);
+//     qInfo() << qs;
+//     auto qdt1 = ut->fromconvertiIGData(s);
+//     qInfo() << qdt1;
+//     auto qs1 = ut->toconvertData(qdt1);
+//     qInfo() << qs;
+//     delete ut;
+// }
+
+// XFUZZ_TEST_ENTRYPOINT(test);
+
+
+#include <QCoreApplication>
+#include <DLog>
+#include <QDBusConnection>
+#include <QDBusError>
+#include <QTranslator>
 #include <QDebug>
-#include <QByteArray>
-#include "utils.h"
-#include <QJsonDocument>
-#include <QJsonArray>
+#include "calendarscheduler.h"
+#include <random>
 #include "xfuzz.h"
 
-//自定义转换函数，输入是任意数量的支持的类型（包括其他自定义类型）
+
 QString to_qstring(std::string &s) {
     return QString(s.c_str());
 }
 
 XFUZZ_CUSTOM_CONVERTER(to_qstring);
 
-void test(QString &s) {
-    qInfo() << s;
-    Utils * ut = new Utils();
-    auto qdt = ut->fromconvertData(s);
-    qInfo() << qdt;
-    auto qs = ut->toconvertData(qdt);
-    qInfo() << qs;
-    auto qdt1 = ut->fromconvertiIGData(s);
-    qInfo() << qdt1;
-    auto qs1 = ut->toconvertData(qdt1);
-    qInfo() << qs;
-    delete ut;
+qint64 to_qint64(long int x) {
+    return static_cast<qint64>(x);
 }
 
-XFUZZ_TEST_ENTRYPOINT(test);
+XFUZZ_CUSTOM_CONVERTER(to_qint64);
+
+qint32 to_qint32(int x) {
+    return static_cast<qint32>(x);
+}
+
+XFUZZ_CUSTOM_CONVERTER(to_qint32);
+
+
+QCoreApplication *app;
+
+
+void test_init(){
+    qputenv("QT_QPA_PLATFORM", "offscreen");
+    static char* argv[] = {"./dde-calendar-service-xfuzz", nullptr};
+    int argc = 1;
+    app = new QCoreApplication(argc,argv);
+}
+
+    // QString GetType(qint64 id);
+    // QString GetTypes();
+    // void DeleteJob(qint64 id);
+    // void DeleteType(qint64 id);
+    // QString GetJob(qint64 id);
+    // qint64 CreateJob(const QString &jobInfo);
+    // void UpdateJob(const QString &jobInfo);
+    // void UpdateType(const QString &typeInfo);
+    // QString GetJobs(const QDateTime &start, const QDateTime &end);
+    // QString QueryJobs(const QString &params);
+    // QString QueryJobsWithLimit(const QString &params, qint32 maxNum);
+    // QString QueryJobsWithRule(const QString &params, const QString &rules);
+
+
+void testCalendarScheduler(int caseId, qint64 id, QString jobInfo, QString typeInfo, QString startTime, QString endTime, QString params, qint32 maxNum, QString rules){
+    CalendarScheduler * cs = new CalendarScheduler;
+
+    // std::random_device r;
+    // std::default_random_engine e1(r());
+    // std::uniform_int_distribution<int> uniform_dist(0, 13);
+    // for (int i = 0; i < 26; i++)
+    // {
+    //  caseId = uniform_dist(e1);
+        if (caseId == 1){
+            QString varType = cs->GetType(id);
+            qInfo() << "varType: " << varType;
+        }
+        if (caseId == 2){
+            QString varTypes = cs->GetTypes();
+            qInfo() << "varTypes: " << varTypes;
+        }
+        if (caseId == 3){
+            QString varQueryJobs = cs->QueryJobs(params);
+            qInfo() << "varQueryJobs: " << varQueryJobs;
+        }
+        if (caseId == 4){
+            cs->DeleteJob(id);
+        }
+        if (caseId == 5){
+            cs->DeleteType(id);
+        }
+        if (caseId == 6){
+            QString varJobId = cs->GetJob(id);
+            qInfo() << "varJobId: " << varJobId;
+        }
+        if (caseId == 7){
+            qint64 varCreateJob = cs->CreateJob(jobInfo);
+            qInfo() << "varCreateJob: " << varCreateJob;
+        }
+        if (caseId == 8){
+            cs->UpdateJob(jobInfo);
+        }
+        if (caseId == 9){
+            cs->UpdateType(typeInfo);
+        }
+        if (caseId == 10){
+            QDateTime start = QDateTime::fromString(startTime, "yyyy-MM-ddThh:mm:ss");
+            QDateTime end = QDateTime::fromString(endTime, "yyyy-MM-ddThh:mm:ss");
+            QString varJobs = cs->GetJobs(start, end);
+            qInfo() << "varJobs: " << varJobs;
+        }
+        if (caseId == 11){
+            QString varQueryJobsWithLimit = cs->QueryJobsWithLimit(params, maxNum);
+            qInfo() << "varQueryJobsWithLimit: " << varQueryJobsWithLimit;
+        }
+        if (caseId == 12){
+            QString varQueryJobsWithRule = cs->QueryJobsWithRule(params, rules);
+            qInfo() << "varQueryJobsWithRule: " << varQueryJobsWithRule;
+        }
+    //}
+
+    delete cs;
+}
+
+
+XFUZZ_TEST_ENTRYPOINT_WITH_INIT(
+    testCalendarScheduler, 
+    test_init
+);
 
 
 
